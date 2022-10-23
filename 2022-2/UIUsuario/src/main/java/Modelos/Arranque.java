a2e8f2efebf05baa2b54dfe76478607aea6ed8a3
Cliqui-robot <roboticaeducativapucv@gmail.com>
2022-10-23 18:34:12 -0300

    ejemplo EP3

---
+++ 2022-2/UIUsuario/src/main/java/Modelos/Arranque.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author sandracano
+ */
+public class Arranque {
+    
+    
+    
+    public Arranque(){
+        
+    }
+    
+    public void Menu(){
+        System.out.println("1- Crear Persona");
+        System.out.println("2- Eliminar Persona");
+        
+        
+        
+        
+        
+    }
+    
+}
